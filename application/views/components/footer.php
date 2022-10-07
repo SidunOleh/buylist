66d26fe3babaecdc3c08eb152005035f48224ea6
Oleh Sidun <sidunoleh@gmail.com>
2022-10-07 18:05:40 +0300

    buylist

--- application/views/components/footer.php
+++ application/views/components/footer.php
@@ -11,7 +11,7 @@
 </div>
 
 <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.2.0/dist/js/bootstrap.min.js"></script>
 <script src="/js/script.js"></script>
 </body>
 
