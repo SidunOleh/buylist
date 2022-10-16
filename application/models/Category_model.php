c9d5c729e7c0d9137c55d9dd9690a8bb4e28ebbb
Oleh Sidun <sidunoleh@gmail.com>
2022-10-16 11:42:40 +0300

    new

--- application/models/Category_model.php
+++ application/models/Category_model.php
@@ -6,9 +6,7 @@ class Category_model extends CI_Model
     public $name;
 
     public function index()
-    {
-        $result = $this->db->get('categories');
-        
+    {    
         return $result->custom_result_object('Category_model');
     }
 
